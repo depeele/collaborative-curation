391bc6c579e680c7718be4ea15f0f27c4f31e552
D. Elmo Peele <depeele@tycho.ncsc.mil>
2012-07-17 15:05:01 -0400

    Better handling of externally dropped items.
    lib/main-sidebar.js
        Allow data url retrievals to complete before firing a newly generated
        'dropExternal' event.
    
    data/view/topics/topics-sidebar.js
        Add TopicItem::visitItem() click handler to allow more specific creation of
        the target URL based upon an item's url and selector.
    
        In dataTransfer2items(), for 'text/html', include the url of the source
        page (provided by the add-on) as well as a selector.
    
    data/view/topics/topics.css
        Adjust the presentation of the date/timestamp for topic items.

--- data/view/topics/topics-sidebar.js
+++ data/view/topics/topics-sidebar.js
@@ -176,7 +176,7 @@ var TopicsView  = Backbone.View.extend({
                 (e.ctrlKey  ? ' ' : '!'),
                 (e.metaKey  ? ' ' : '!'));
 
-        // Post that we're ready
+        // Request a visit to the target href.
         addon.postMessage({
             src:    'sidebar-content',
             action: 'visit',
@@ -400,7 +400,7 @@ var TopicView   = Backbone.View.extend({
             $src            = gDragging,
             canDrop         = (dataTransfer && self.canDrop($src));
 
-        console.log("TopicView::dragOver()");
+        //console.log("TopicView::dragOver()");
 
         if (! canDrop)  { return; }
 
@@ -623,7 +623,8 @@ var ItemView    = Backbone.View.extend({
     className:  'curation-item',
 
     events:     {
-        'render':                       'render'
+        'click a':  'visitItem',
+        'render':   'render'
     },
 
     template:   '#curation-item',
@@ -688,12 +689,31 @@ var ItemView    = Backbone.View.extend({
         self.$el.html( self.template(item) );
 
         return self;
-    }
+    },
 
     /************************************************************************
      * Event handlers
      *
      */
+
+    visitItem: function(e) {
+        var self    = this;
+
+        console.log("ItemView::visitItem(): item[ %s ]",
+                    JSON.stringify(self.options.model));
+
+        e.preventDefault();
+        e.stopPropagation();
+
+        // Request a visit to the target href.
+        addon.postMessage({
+            src:        'sidebar-content',
+            action:     'visit',
+            url:        self.options.model['url'],
+            selector:   self.options.model['selector'],
+            current:    (! e.metaKey)
+        });
+    }
 });
 
 $(document).ready(function() {
@@ -796,38 +816,38 @@ function sprintf(fmt, args)
                 {
                 // String
                 case 's':
-                    formatted = arg;
+                    formatted = (arg ? arg : '');
                     break;
 
                 // Integer
                 case 'd':
-                    formatted = parseInt(arg, 10);
+                    formatted = (arg ? parseInt(arg, 10) : formatted);
                     break;
 
                 case 'x':
-                    formatted = parseInt(arg, 16);
+                    formatted = (arg ? parseInt(arg, 16) : formatted);
                     break;
 
                 case 'o':
-                    formatted = parseInt(arg, 8);
+                    formatted = (arg ? parseInt(arg, 8) : formatted);
                     break;
 
                 // Floating point
                 case 'f':
                 case 'g':
-                    formatted = parseFloat(arg);
+                    formatted = (arg ? parseFloat(arg) : formatted);
                     break;
 
                 // JSON
                 case 'j':
-                    formatted = JSON.stringify(arg);
+                    formatted = (arg ? JSON.stringify(arg) : formatted);
                     break;
                 }
             } catch(e) {
                 formatted = "**Format Error: "+ e.message;
             }
 
-            str += (formatted ? formatted.toString() : '');
+            str += (formatted.toString ? formatted.toString() : '');
             pos += match.length;
         }
     }
@@ -860,10 +880,11 @@ function sprintf(fmt, args)
 function dataTransfer2Items(topic, dataTransfer)
 {
     /* Type-based Heuristic:
-     *  - 'application/x-moz-file' (or dataTransfer.files.length > 0)
-     *      dropping an external file from the system
-     *          use dataTransfer.files
-     *              {size, type, name, mozFullPath}
+     *  - one or more 'files'
+     *      dropping an external/system file
+     *          generate a URL to the target file and, if the dataTransfer item
+     *          includes a 'dataUrl' item, include an img using the dataUrl as
+     *          the source;
      *  - 'text/x-moz-url' but no 'text/_moz_htmlcontext'
      *      dropping a URL from the address bar
      *          use 'text/x-moz-url', splitting the URL from the title
@@ -871,7 +892,11 @@ function dataTransfer2Items(topic, dataTransfer)
      *      dropping a bookmark entry {title, uri}
      *          use 'text/html'
      *  - 'text/html'
-     *      dropping pre-formated HTML -- use it directly;
+     *      dropping pre-formated HTML
+     *          if the data is an object with an 'html' member, assume sbDrop()
+     *          has converted it to an item of the form:
+     *              {html:, srcUrl}
+     *          otherwise, use it directly as raw html;
      *  - 'text/plain'
      *      dropping plain-text -- ignore??;
      */
@@ -880,37 +905,32 @@ function dataTransfer2Items(topic, dataTransfer)
             return (dataTransfer.types.indexOf(val) >= 0);
         };
 
-    /*
-    if (hasType('application/x-moz-file'))
-    {
-        var data    = dataTransfer['application/x-moz-file'];
-
-        // Create an entry for each file
-        console.log("dataTransfer2Items:   %d: application/x-moz-file entries:",
-                    data.length);
-
-        _.each(data, function(file, idex) {
-            console.log("dataTransfer2Items:    %d: %j", idex, file);
-        });
-    }
-    else
-    // */
-
     if (dataTransfer.files && (dataTransfer.files.length > 0))
     {
         // Create an entry for each file
+
+        /*
         console.log("dataTransfer2Items:   %d: file entries:",
             dataTransfer.files.length);
+        // */
 
         _.each(dataTransfer.files, function(file, idex) {
-            console.log("dataTransfer2Items:    %d: %j", idex, file);
+            //console.log("dataTransfer2Items:    %d: %j", idex, file);
 
+            /* If this item has a 'dataUrl', use it in an enclosed <img>
+             * element to provide a file-independent view of the file contents
+             * that were in place when the file was dropped.
+             */
             var url     = 'file://'+ file.path,
-                title   = file.name;
+                content = '<a href="'+ url +'">'
+                        +   (file.dataUrl
+                                ? '<img src="'+ file.dataUrl +'" />'
+                                : file.name)
+                        + '</a>';
 
             items.push({
                 timestamp:  (new Date()).getTime(),
-                content:    '<a href="'+ url +'">'+ title +'</a>',
+                content:    content,
                 url:        url,
                 selector:   '',
                 topicId:    topic.id,
@@ -927,7 +947,10 @@ function dataTransfer2Items(topic, dataTransfer)
             title       = data.title,
             selector    = '';
 
-        console.log("dataTransfer2Items:   text/x-moz-place: data[ %j ]", data);
+        /*
+        console.log("dataTransfer2Items:   text/x-moz-place: data[ %j ]",
+                    data);
+        // */
 
         items.push({
             timestamp:  (new Date()).getTime(),
@@ -947,12 +970,14 @@ function dataTransfer2Items(topic, dataTransfer)
          */
         var data        = dataTransfer['text/x-moz-url'];
 
+        /*
         console.log("dataTransfer2Items:   %d: text-x-moz-urls "
                     +   "without _moz_htmlcontext",
                     data.length);
+        // */
 
         _.each(data, function(entry, idex) {
-            console.log("dataTransfer2Items:      %d: %j", idex, entry);
+            //console.log("dataTransfer2Items:      %d: %j", idex, entry);
 
             items.push({
                 timestamp:  (new Date()).getTime(),
@@ -967,28 +992,127 @@ function dataTransfer2Items(topic, dataTransfer)
     }
     else if (hasType('text/html'))
     {
-        /* Use the dropped HTML
-         *
-         *  :TODO: Grab the page URL (via tabs)
-         *         and generate the page selector.
+        // Use the dropped HTML (or sbDrop() constructed item).
+        var item        = dataTransfer['text/html'],
+            html        = (item && item.html
+                            ? item.html
+                            : item),
+            $html       = $('<div>'+ html +'</div>'),
+            url         = (item && item.srcUrl
+                            ? item.srcUrl.replace(/#.*$/, '')
+                            : 'url://of.source/page'),
+            selector    = '';   //'#content > .selector';
+
+        /*
+        console.log("dataTransfer2Items:   text/html: item[ %s ]",
+                    JSON.stringify(item));
+        // */
+
+        var $id = $html.find('[id]').last();
+        if ($id && ($id.length > 0))
+        {
+            /* Use the id of the *last* element in the incoming HTML that has
+             * an id
+             */
+            selector = '#'+ $id.attr('id');
+        }
+        else if ( hasType('text/_moz_htmlcontext'))
+        {
+            // Use _moz_htmlcontext to construct a selector
+            var context     = dataTransfer['text/_moz_htmlcontext'],
+                $context    = $(context),
+                $inner      = $context.find(':not(:has(*))').last(),
+                path        = [];
+
+            /*
+            console.log("dataTransfer2Items:   text/html: context[ %s ]",
+                        context);
+            console.log("dataTransfer2Items:   text/html: html[ %s ]",
+                        html);
+            // */
+
+            $inner.append( $html.children(':first').clone() );
+
+            /* Use the id of the *last* element in our constructed context that
+             * has an id
+             */
+            $id = $context.find('[id]').last();
+            if ($id && ($id.length > 0))
+            {
+                selector = '#'+ $id.attr('id');
+            }
+        }
+
+        /* Remove any in-line styling and event handlers from the incoming HTML
+         * elements
          */
-        var data        = dataTransfer['text/html'],
-            url         = 'url://of.source/page',
-            selector    = '#content > .selector';
+        $html.find('*').each(function() {
+            var el  = this,
+                $el = $(el);
 
-        console.log("dataTransfer2Items:   text/html: data[ %s ]", data);
+            /* Walk through the properties and remove any in-line styling and
+             * event handlers
+             */
+            var toRemove    = [];
+            for (var prop in el)
+            {
+                if ((! $el.attr(prop)) && (! $el.prop(prop)))   { continue; }
 
-        items.push({
+                if (prop.match(/^(style$|on|height|width)/i))
+                {
+                    toRemove.push(prop);
+                }
+            }
+
+            toRemove.forEach(function(name) {
+                $el.removeProp(name);
+                $el.removeAttr(name);
+            });
+        });
+
+        /*
+        console.log("dataTransfer2Items:   text/html: html[ %s ]", html);
+        console.log("dataTransfer2Items:   text/html: $html[ %s ]",
+                        $html.html());
+        // */
+
+        var item    = {
             timestamp:  (new Date()).getTime(),
-            content:    data,
+            content:    $html.html().trim(),
             url:        url,
             selector:   selector,
             topicId:    topic.id,
             order:      '',
             comments:   []
-        });
+        };
+
+        // /*
+        console.log("dataTransfer2Items:   text/html: item[ %s ]",
+                    JSON.stringify(item));
+        // */
+
+        items.push( item );
     }
     // else, IGNORE (by NOT adding anyting to items)
+
+    /* 'x-moz-file' ends up having *less* information than simple File (no
+     * 'type' and no possibility of 'dataUrl'), so we ignore it here ...
+     *
+    if (hasType('application/x-moz-file'))
+    {
+        var data    = dataTransfer['application/x-moz-file'];
+
+        // Create an entry for each file
+        console.log("dataTransfer2Items:   %d: application/x-moz-file entries:",
+                    data.length);
+
+        _.each(data, function(file, idex) {
+            console.log("dataTransfer2Items:    %d: %j", idex, file);
+        });
+    }
+    else
+    // */
+
 
     return items;
 }
